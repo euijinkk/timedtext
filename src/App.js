4b85cee34879476e1d91cfc7f2c4e01868dbf661
gytks4 <gytks4@naver.com>
2021-06-24 15:32:30 +0900

    feat: control UI 추가

--- src/App.js
+++ src/App.js
@@ -3,14 +3,33 @@ import React, { useCallback, useMemo, useRef } from 'react';
 import { useState } from 'react';
 import { useEffect } from 'react';
 import X2JS from 'x2js';
-import { Media, Player, controls, utils } from 'react-media-player';
-import styled from 'styled-components'
-import { useGetRecoilValueInfo_UNSTABLE } from 'recoil';
+import { Media, Player, controls } from 'react-media-player';
+import {
+  PlayPause,
+  CurrentTime,
+  Progress,
+  SeekBar,
+  Duration,
+  MuteUnmute,
+  Volume,
+  Fullscreen,
+} from 'react-media-player/lib/controls';
+import styled from 'styled-components';
+// import { CurrentTime } from 'react-media-player/lib/controls';
 
 const App = () => {
   const [tt, setTt] = useState();
   const [currentTime, setCurrentTime] = useState();
-  const { PlayPause, MuteUnmute } = controls;
+  // const {
+  //   PlayPause,
+  //   CurrentTime,
+  //   Progress,
+  //   SeekBar,
+  //   Duration,
+  //   MuteUnmute,
+  //   Volume,
+  //   Fullscreen,
+  // } = controls;
   const playerRef = useRef();
 
   const getTimedText = useCallback(async () => {
@@ -38,22 +57,33 @@ const App = () => {
 
   const handleLyrics = (line) => {
     playerRef.current.context.media.seekTo(parseFloat(line._start));
-  }
+  };
   return (
     <YoutubeWrapper>
       <Media>
         <div className="media">
-          <div className="media-player">
+          <div className="media-player" display="none">
             <Player
               src="https://www.youtube.com/embed/o_SYttJm0SE"
               vendor="youtube"
               onTimeUpdate={handleTimeUpdate}
               ref={playerRef}
+              style={{ display: 'none' }}
             />
           </div>
           <div className="media-controls">
-            <PlayPause />
-            <MuteUnmute />
+            <div className="top">
+              <CurrentTime2 />
+              {/* <Progress /> */}
+              <SeekBar2 />
+              <Duration />
+            </div>
+            <div className="bottom">
+              <PlayPause />
+              {/* <MuteUnmute /> */}
+              <Volume2 />
+              {/* <Fullscreen /> */}
+            </div>
           </div>
         </div>
         {/* <iframe
@@ -73,7 +103,7 @@ const App = () => {
           <div
             key={index}
             onClick={() => handleLyrics(line)}
-            className={"lyrics " + (currentTime === line._start && 'highlight')}
+            className={'lyrics ' + (currentTime === line._start && 'highlight')}
           >
             {line.__text}
           </div>
@@ -84,16 +114,36 @@ const App = () => {
 
 export default App;
 
+const CurrentTime2 = styled(CurrentTime)`
+  /* font-size:px; */
+`;
+
+const SeekBar2 = styled(SeekBar)`
+  /* font-size:px; */
+  width: 300px;
+`;
+
+const Volume2 = styled(Volume)`
+  /* font-size:px; */
+`;
+
 const YoutubeWrapper = styled.div`
-display:flex;
-flex-direction:column;
-  .lyrics{
+  
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  
+  .media{
+    margin: 50px 0;
+  }
+  .lyrics {
     line-height: 30px;
-    font-size:16px;
-    text-align:center;
+    font-size: 16px;
+    text-align: center;
+    cursor: pointer;
   }
 
-  .highlight{
-    color:red;
+  .highlight {
+    color: red;
   }
-`
+`;
