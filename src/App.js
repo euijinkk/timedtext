3d063af03d0ecf9bc3de6f266dc23c623fdd5a21
gytks4 <gytks4@naver.com>
2021-06-24 14:08:33 +0900

    feat: 유튜브 영상 임베드, 가사 하이라이트, 가사 클릭 구현

--- src/App.js
+++ src/App.js
@@ -1,18 +1,99 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { MainPage, LoginPage, MyPage, RegisterPage } from './pages';
+import axios from 'axios';
+import React, { useCallback, useMemo, useRef } from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import X2JS from 'x2js';
+import { Media, Player, controls, utils } from 'react-media-player';
+import styled from 'styled-components'
+import { useGetRecoilValueInfo_UNSTABLE } from 'recoil';
 
 const App = () => {
+  const [tt, setTt] = useState();
+  const [currentTime, setCurrentTime] = useState();
+  const { PlayPause, MuteUnmute } = controls;
+  const playerRef = useRef();
+
+  const getTimedText = useCallback(async () => {
+    const timedText = await axios.get(
+      'https://video.google.com/timedtext?lang=ko&v=o_SYttJm0SE',
+    );
+    var x2js = new X2JS();
+    setTt(x2js.xml2js(timedText.data).transcript.text);
+  }, []);
+
+  useEffect(() => {
+    getTimedText();
+  }, []);
+
+  const handleTimeUpdate = (event) => {
+    tt.forEach((line) => {
+      if (
+        parseFloat(line._start) <= event.currentTime &&
+        event.currentTime < parseFloat(line._start) + parseFloat(line._dur)
+      ) {
+        setCurrentTime(line._start);
+      }
+    });
+  };
+
+  const handleLyrics = (line) => {
+    playerRef.current.context.media.seekTo(parseFloat(line._start));
+  }
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={()=> <MainPage />} />
-        <Route exact path="/login" component={()=> <LoginPage />} />
-        <Route exact path="/register" component={()=> <RegisterPage />} />
-        <Route exact path="/my" component={()=> <MyPage />} />
-      </Switch>
-    </Router>
+    <YoutubeWrapper>
+      <Media>
+        <div className="media">
+          <div className="media-player">
+            <Player
+              src="https://www.youtube.com/embed/o_SYttJm0SE"
+              vendor="youtube"
+              onTimeUpdate={handleTimeUpdate}
+              ref={playerRef}
+            />
+          </div>
+          <div className="media-controls">
+            <PlayPause />
+            <MuteUnmute />
+          </div>
+        </div>
+        {/* <iframe
+      className="video"
+      type="text/html"
+      title="youtuve video player"
+      width="100%"
+      height="500px"
+      src="https://www.youtube.com/embed/o_SYttJm0SE"
+      frameBorder="0"
+      allowFullScreen
+      onTimeUpdate={handleTimeUpdate}
+    ></iframe> */}
+      </Media>
+      {tt &&
+        tt.map((line, index) => (
+          <div
+            key={index}
+            onClick={() => handleLyrics(line)}
+            className={"lyrics " + (currentTime === line._start && 'highlight')}
+          >
+            {line.__text}
+          </div>
+        ))}
+    </YoutubeWrapper>
   );
 };
 
 export default App;
+
+const YoutubeWrapper = styled.div`
+display:flex;
+flex-direction:column;
+  .lyrics{
+    line-height: 30px;
+    font-size:16px;
+    text-align:center;
+  }
+
+  .highlight{
+    color:red;
+  }
+`
